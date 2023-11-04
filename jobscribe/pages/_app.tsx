86e0ea672521d573b087e028e444501c358faa53
Sora-210 <sokominto1231@gmail.com>
2023-11-04 22:47:04 +0900

    add: 全体的なデザイン変更

--- jobscribe/pages/_app.tsx
+++ jobscribe/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import styled from 'styled-components'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,7 +11,16 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="description" content="時間を記録して効率よくtoDoをこなすお手伝い!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Component {...pageProps} />
+      <Container>
+        <Component {...pageProps} />
+      </Container>
     </>
   )
 }
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #F3F3F3;
+  orverflow-y: scroll;
+`
